fa70e88241e97c507640010743e567458eb5466b
Brent Cameron <bjcameron@gmail.com>
2020-07-29 14:25:18 -0600

    add create action to controller and view for it

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,13 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
 
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+        @article.save
+        redirect_to article_path(@article)
+    end 
 end
